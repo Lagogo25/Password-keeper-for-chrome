aa7fdb0d830b1fd79115c32776f1deda918d301b
Lagogo20 <lagogo@gmail.com>
2017-06-17 18:10:01 +0300

    changed user interface (popup) and added a lot of stuff!! (check changes yourself ;))

--- background.js
+++ background.js
@@ -1,29 +1,38 @@
-
 var socket = io('http://localhost:1337/');
-var delivery = new Delivery(socket);
+
 var connected = false;
-var user = "";
+var user = ""; // use to show message for popup
+var password = ""; // use to encrypt and decrypt info
+var salt = ""; // needed to encrypt and decrypt. key will be password + salt
 var resp;
-
-delivery.on('receive.start',function(fileUID){
-    console.log('receiving a file!');
-});
-
-delivery.on('receive.success',function(file){
-    console.log("receive success!");
-    console.log(file.text());
-    // if (file.isImage()) {
-    //     $('img').attr('src', file.dataURL());
-    // }
-});
 
 socket.on('server_ready', function (data) {
     chrome.runtime.onMessage.addListener(function(msg){
-        user = msg.user;
-        var pass = msg.password;
         var req = msg.request;
-        socket.emit(req, { user: user, password: pass });
+        switch (req){
+            case "login":
+            case "register":
+                user = msg.user;
+                password = msg.password;
+                socket.emit(req, {user: user, password: password});
+                break;
+            case "update_server":
+                connected = false;
+                var fileName, fileContent;
+                chrome.storage.local.get(['fileName', 'fileContent'], function(items){
+                    fileName = items.fileName;
+                    fileContent = items.fileContent;
+                    console.log('file name: ' + fileName + ' and content: ' + fileContent);
+                });
+                socket.emit(req, {fileName: fileName, content: fileContent});
+                chrome.runtime.reload();
+                break;
+            case "add_to_file":
+                console.log("got the message!");
+                break;
+        }
     });
+
 });
 
 socket.on('login_response', function(data){
@@ -36,12 +45,27 @@ socket.on('login_response', function(data){
     }
     if (resp === "signed in") {
         connected = true;
+        salt = data.salt;
+        // save fileName and content on local memory?
+        chrome.storage.local.set({ 'fileName': data.name, 'fileContent': data.content}, function(){
+            console.log('set variables fileName (' + data.name + ') and fileContent (' + data.content + ')');
+        });
         for (var i = 0; i < views.length; i++) {
             views[i].document.getElementById('status').innerHTML = user + " " + resp;
             views[i].displayLogin('none');
+            var fileName, fileContent;
+            chrome.storage.local.get(['fileName', 'fileContent'], function (items) {
+                fileName = items.fileName;
+                fileContent = items.fileContent;
+            });
+            views[i].document.getElementById("sign-out").onclick = function () {
+                // should send file to server
+                connected = false;
+                socket.emit('update_server', {fileName: fileName, content: fileContent});
+                chrome.runtime.reload();
+            };
+            views[i].document.getElementById('sign-out').style.display = 'block';
         }
-        // should now download file!
-
     }
     console.log(resp);
 });
@@ -56,3 +80,43 @@ socket.on('register_response', function(data){
     }
     console.log(resp);
 });
+
+/*
+Now comes the actual password keeping business!!
+ */
+// maybe add this functionality when signing in so that the current tab already load credentials?
+chrome.tabs.onActivated.addListener(function(activeInfo){ // launch whenever we switch tab
+    var tabId = activeInfo.tabId;
+    if (connected) {
+        chrome.tabs.executeScript(tabId, {
+            code: '' +
+            'var html = document.body.outerHTML;' +
+            'chrome.runtime.sendMessage({request: "add_to_file", });' +
+            ''
+        });
+    }
+    // chrome.tabs.onUpdated.addListener(function(tabId, info) { // launch when tab is loaded
+    //     if (info.status === 'complete') {
+    //         if (connected) {
+    //             chrome.tabs.executeScript(tabId, {
+    //                 code: '' +
+    //                 'alert(document.body.outerHTML);' +
+    //                 ''
+    //             });
+    //         }
+    //     }
+    // });
+});
+
+
+
+// chrome.tabs.query({active: true, lastFocusedWindow: true}, function (tabs) {
+//     // tabs[0] is the current actibe tab
+//     if (connected) {
+//         console.log(tabs[0].url);
+//     }
+// });
+
+// chrome.tabs.getCurrent(function(tab){
+//
+// });
--- popup.js
+++ popup.js
@@ -3,6 +3,39 @@ function displayLogin(state){
     document.getElementById("pass-box").style.display = state;
     document.getElementById("buttons").style.display = state;
 }
+
+// for password
+function validPassword(str) {
+    //language=JSRegexp
+    return str.length === 8 &&/^[\x21-\x7E]*$/.test(str);
+}
+
+// for username
+function validUser(str) {
+    //language=JSRegexp
+    return 1 <= str.length && str.length <= 256 && /^[\x30-\x39\x41-\x5A\x61-\x7A]*$/.test(str);
+}
+
+function checkValid(user, password){
+    if (validUser(user)){
+        if (validPassword(password)){
+            return true;
+        }
+        else{ // invalid password
+            document.getElementById("status").innerHTML =
+                '<b>Invalid password!</b><br>' +
+                '(Should be 8 ASCII characters between 0X21 and 0X7E.)';
+            return false;
+        }
+    }
+    else{ // invalid user name
+        document.getElementById("status").innerHTML =
+            '<b>Invalid user name!</b><br>' +
+            '(Should be one word consisted only by letters and numbers.)';
+        return false;
+    }
+}
+
 var background_page = chrome.extension.getBackgroundPage();
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -10,28 +43,62 @@ document.addEventListener('DOMContentLoaded', function() {
         displayLogin('block');
         document.getElementById("sign-in").onclick = function () {
             document.getElementById("sign-in").disabled = true;
-            document.getElementById("sign-in").disabled = true;
-            chrome.runtime.sendMessage({
-                user: document.getElementById("user-name").value,
-                password: document.getElementById("user-password").value,
-                request: 'login'
-            });
+            var uname = document.getElementById("user-name").value;
+            var password = document.getElementById("user-password").value;
+            // check for input validity
+            if (checkValid(uname, password)) {
+                chrome.runtime.sendMessage({
+                    user: uname,
+                    password: password,
+                    request: 'login'
+                });
+            }
             document.getElementById("sign-in").disabled = false;
         };
         document.getElementById("sign-up").onclick = function () {
             document.getElementById("sign-up").disabled = true;
-            chrome.runtime.sendMessage({
-                user: document.getElementById("user-name").value,
-                password: document.getElementById("user-password").value,
-                request: 'register'
-            });
+            var uname = document.getElementById("user-name").value;
+            var password = document.getElementById("user-password").value;
+            // check for input validity
+            if (checkValid(uname, password)) {
+                chrome.runtime.sendMessage({
+                    user: uname,
+                    password: password,
+                    request: 'register'
+                });
+            }
             document.getElementById("sign-up").disabled = false;
         };
     }
-    else
-        {
-            // we are already connected!
-            // should add a sign out option (will send file to server and return to default)
-            document.getElementById('status').innerHTML = background_page.user + " signed in";
+    else {
+        document.getElementById('sign-out').style.display = 'block';
+        // we are already connected!
+        // should add a sign out option (will send file to server and return to default)
+        // on sign out should send file to server, disconnect, and change connected to false (chrome.runtime.reload()?)
+        document.getElementById('status').innerHTML = background_page.user + " signed in.";
+        document.getElementById('sign-out').onclick = function () {
+            // should send file to server
+            chrome.runtime.sendMessage({
+                // should send file to server (will happen in background)
+                request: 'update_server'
+            });
+        };
+    }
+
+    document.getElementById("user-name").addEventListener("keyup", function (event) {
+        // event.preventDefault();
+        if (event.keyCode === 13) {
+            // alert("enter was pressed!");
+            document.getElementById("user-password").focus();
         }
+    });
+
+    document.getElementById("user-password").addEventListener("keyup", function (event) {
+        // event.preventDefault();
+        if (event.keyCode === 13){
+            document.getElementById("sign-in").click();
+        }
+    });
 });
+
+
